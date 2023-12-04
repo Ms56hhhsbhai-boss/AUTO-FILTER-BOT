d6759ea26ab082be9eee5e54f08f31a71a4667e1
deendayal422 <149863092+deendayal422@users.noreply.github.com>
2023-12-04 21:51:05 +0530

    Update info.py

--- info.py
+++ info.py
@@ -27,8 +27,8 @@ MELCOW_VID = environ.get("MELCOW_VID")
 SPELL_IMG = environ.get("SPELL_IMG")
 
 # Admins, Channels & Users
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '10161397503').split()]
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001900525156').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '6646028262').split()]
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002144894787').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 PREMIUM_USER = [int(user) if id_pattern.search(user) else user for user in environ.get('PREMIUM_USER', '').split()]
@@ -61,7 +61,7 @@ CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/Moviesrequest_info')
 TUTORIAL = environ.get('TUTORIAL', 'https://t.me/How_to_open_Mdisk_pro_links/2')
 IS_TUTORIAL = bool(environ.get('IS_TUTORIAL', True))
 MSG_ALRT = environ.get('MSG_ALRT', 'Hello Everyone ❤️')
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001804171666'))
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002090431700'))
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'Moviesrequest_chat')
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "False")), False)
 IMDB = is_enabled((environ.get('IMDB', "True")), True)
