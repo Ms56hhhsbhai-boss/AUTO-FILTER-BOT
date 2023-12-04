a7b379940e227791ef56d9eaff146a2cd7ac67c8
deendayal422 <149863092+deendayal422@users.noreply.github.com>
2023-12-04 21:53:39 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/wishvishnu179/ADVFILTERBOT.git /ADVFILTERBOT 
+  git clone https://github.com/deendayal422/AUTO-FILTER-BOT.git /AUTO-FILTER-BOT 
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /ADVFILTERBOT 
+  git clone $UPSTREAM_REPO /AUTO-FILTER-BOT 
 fi
-cd /ADVFILTERBOT
+cd /AUTO-FILTER-BOT
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
